e1a7b79478bbb3739357dd536e8fecc84044f944
oceanborn2 <pascal.munerot@gmail.com>
2021-10-03 20:33:23 +0200

    added new C calls to test EPD

--- core/build.sh
+++ core/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -ldflags="-extldflags '-L../clib -lm -lepd'"
+mach=`uname -m`
+if [[ $mach -eq 'armv6l' ]]; then
+	GOOS=linux GOARCH=amd64 go build -ldflags="-extldflags '-L../clib -lm -lepd'"
+else
+	GOOS=linux GOARCH=arm i go build -ldflags="-extldflags '-L../clib -lm -lepd'" && scp librarius pi@plume:~/librarius/core
+fi
 
-GOOS=linux GOARCH=arm go build && scp librarius pi@plume:~/librarius/core
+
 
 
